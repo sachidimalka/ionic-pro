89d87f24298affb52e1059bced19046ae2593922
sachidimlaka <sachinisiriwardhana07@gmail.com>
2022-03-14 17:03:34 +0530

    todo list add task

--- photo-gallery/src/app/tab3/tab3.page.ts
+++ photo-gallery/src/app/tab3/tab3.page.ts
@@ -5,7 +5,10 @@ interface Todo {
   time: string;
 }
 
-
+interface Task {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -34,8 +37,35 @@ export class Tab3Page {
     }
   ];
 
+  task: Task[] = [
+    {
+      id: 1,
+      name: 'java',
+    },
+    {
+      id: 2,
+      name: 'scala',
+    },
+    {
+      id: 3,
+      name: 'php',
+    }
+  ];
+
   compareWith(o1: Todo, o2: Todo) {
     return o1 && o2 ? o1.id === o2.id : o1 === o2;
   }
+  taskName: any = ''; // Entered Text
+  taskList = []; // Array to store tasks
 
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = '';
+    }
+  }
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
+  }
 }
