6fef11680d784213c4cc08d197b716d12dd8eca2
sachidimlaka <sachinisiriwardhana07@gmail.com>
2022-03-14 16:35:05 +0530

    todo-list component

--- photo-gallery/src/app/tab3/tab3.page.ts
+++ photo-gallery/src/app/tab3/tab3.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+interface Todo {
+  id: number;
+  name: string;
+  time: string;
+}
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
+
 export class Tab3Page {
 
-  constructor() {}
+  todo: Todo[] = [
+    {
+      id: 1,
+      name: 'IT assignments',
+      time: 'Smith',
+    },
+    {
+      id: 2,
+      name: 'Physics assignments',
+      time: 'Davis',
+    },
+    {
+      id: 3,
+      name: 'Mathz home work',
+      time: 'Rosenburg',
+    }
+  ];
+
+  compareWith(o1: Todo, o2: Todo) {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  }
 
 }
